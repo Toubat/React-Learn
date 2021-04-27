21673e145db218d141c0e34c434ac072ed8f34c9
Toubat <57741529+Toubat@users.noreply.github.com>
2021-04-27 08:20:29 -0400

    add navbar

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,50 +1,31 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-    imageUrl: "https://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"],
-  };
-
   styles = {
     fontSize: 20,
     fontWeight: "bold",
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) {
-      return <p>There are no tags</p>;
-    } else {
-      return (
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      );
-    }
-  }
-
-  handleIncrement = (e) => {
-    console.log(e);
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
-    // console.log("props", this.props);
+    console.log("props", this.props);
     // React.createElement
     return (
-      <div style={{ paddingBottom: 5 }}>
+      <div>
+        {this.props.children}
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
-          style={{ fontSize: 30 }}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
@@ -52,12 +33,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -4,9 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import Counters from "./components/counters";
 
-ReactDOM.render(<Counters />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
