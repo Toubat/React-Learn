30ba548bd62e7142aa4e17aad4069905a5c7ba81
Toubat <57741529+Toubat@users.noreply.github.com>
2021-04-28 17:22:34 -0400

    add Router

--- router-app/src/components/admin/dashboard.jsx
+++ router-app/src/components/admin/dashboard.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import SideBar from "./sidebar";
+import Users from "./users";
+import Posts from "./posts";
+import { Route } from "react-router-dom";
 
 const Dashboard = ({ match }) => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+      <SideBar />
+      <Route path="/admin/users" component={Users} />
+      <Route path="/admin/posts" component={Posts} />
     </div>
   );
 };
--- router-app/src/components/posts.jsx
+++ router-app/src/components/posts.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import queryString from "query-string";
 
-const Posts = () => {
+const Posts = ({ match, location }) => {
+  const result = queryString.parse(location.search);
   return (
     <div>
       <h1>Posts</h1>
-      Year: , Month:
+      Year: {match.params.year}, Month: {match.params.month}
     </div>
   );
 };
